682538c82baac50b34873d415618a248f335ef2c
kadirkurhan <kadirkurhan42@hotmail.com>
2023-06-21 22:02:36 +0300

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "20b5806f617f7f02a19591cec15cb363",
-"index.html": "d9bdf8b017d415f37f7af6bb8a819243",
-"/": "d9bdf8b017d415f37f7af6bb8a819243",
+"index.html": "18630220f4f2ac3f1b0af353409979ff",
+"/": "18630220f4f2ac3f1b0af353409979ff",
 "main.dart.js": "80b17c07ad94268dcd243ebc01120e43",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
